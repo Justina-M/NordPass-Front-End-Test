615c64019215641799fd48660e9a242a37a69ff6
Justina <justina.miklyciute@gmail.com>
2022-03-31 23:56:29 +0300

    Use downloaded fonts instead of cdn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
+import "./fonts/Gordita Bold.woff";
+import "./fonts/Gordita Medium.woff";
+import "./fonts/Gordita Regular.woff";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
